8f17802330e9b4fe753aaa7c3320295cdab1b944
mosm <airman515@gmail.com>
2017-05-21 03:08:15 +0900

    ログの排他制御・ファイルへの出力

--- islands/include/Log.h
+++ islands/include/Log.h
@@ -4,9 +4,53 @@
 
 namespace islands {
 
-const char* getFormattedTime();
+class Logger : public std::ostream {
+public:
+	virtual ~Logger() = default;
 
-#define SLOG std::clog << ::islands::getFormattedTime() << \
+	static std::unique_lock<std::mutex> lock() {
+		static std::mutex mutex;
+		return std::unique_lock<std::mutex>(mutex);
+	}
+
+	static Logger& getInstance() {
+		static Logger instance;
+		return instance;
+	}
+
+private:
+	class LogBuffer : public std::stringbuf {
+	public:
+		LogBuffer() : ofs_("islands.log") {}
+
+		virtual ~LogBuffer() {
+			pubsync();
+		}
+
+		int sync() override {
+			const auto time = sys::getTime();
+			static char buf[32];
+			strftime(buf, sizeof(buf), "%Y/%m/%d %I:%M:%S", &time);
+
+			std::clog << buf << str();
+			std::clog.flush();
+
+			ofs_ << buf << str();
+			ofs_.flush();
+
+			str("");
+			return 0;
+		}
+
+	private:
+		std::ofstream ofs_;
+	} buffer_;
+
+	Logger() : std::ostream(&buffer_) {}
+};
+
+#define SLOG ::islands::Logger::lock(), ::islands::Logger::getInstance() << \
 	" [" << ::islands::sys::basename(__FILE__) << ":" << __LINE__ << "] "
+
 
 }
