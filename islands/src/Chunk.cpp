f97586aac9778c0aa68a18ca3facce6b2d87b441
mosm <airman515@gmail.com>
2017-10-06 04:58:19 +0900

    Chunk: Read collision option

--- islands/src/Chunk.cpp
+++ islands/src/Chunk.cpp
@@ -138,19 +138,20 @@ void Chunk::loadImpl() {
 
 			entity->attachComponent(drawer);
 
-			//if (modelProp.find("collision") != modelProp.end()) {
-			{
-				//const auto& type = modelProp.at("collision").get<std::string>();
+			if (properties.find("collision") != properties.end()) {
+				const auto& type = properties.at("collision").get<std::string>();
 
-				if (entity->getName() == "Plane.003") {
-					const auto collider = std::make_shared<PlaneCollider>(model, glm::vec3(0.f, 0.f, 1.f));
-					collider->setOffset(2.2f);
-					physicsSystem_.registerCollider(collider);
-					entity->attachComponent(collider);
-				} else {
+				if (type == "sphere") {
 					const auto collider = std::make_shared<SphereCollider>(model);
 					physicsSystem_.registerCollider(collider);
 					entity->attachComponent(collider);
+				} else if (type == "floor") {
+					const auto collider = std::make_shared<PlaneCollider>(model, glm::vec3(0.f, 0.f, 1.f));
+					collider->setOffset(1.5f);
+					physicsSystem_.registerCollider(collider);
+					entity->attachComponent(collider);
+				} else {
+					throw std::exception("not implemented");
 				}
 			}
 		}
